11c472b8e7bb9a73e9a3ff235f039c996764c32b
Yacine S. <hi@yacinesalhi.fr>
2025-09-20 12:01:29 +0200

    tailwindcss install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://your-domain.com',
   integrations: [
     mdx(),
     sitemap()
-  ]
+  ],  // ← virgule ajoutée ici
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
